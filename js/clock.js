9f5ad959d4612a081c5acd583cf212704c1fe32a
Jan Rodeheger <xlshallox@gmail.com>
2019-02-07 20:58:36 +0100

    added bootstrap and jquery for neater design

--- js/clock.js
+++ js/clock.js
@@ -1,16 +1,17 @@
-var correct;
-var result = document.getElementById("result");
+let correct;
+let resultPositive = $("#correct");
+let resultNegative = $("#wrong");
 
 function showTime(hour, minute) {
-	document.getElementById("hhand").style.transform = 'rotateZ(' + ((((hour % 12) * 30) % 360) + (minute / 2)) + 'deg)';
-	document.getElementById("mhand").style.transform = 'rotateZ(' + (minute * 6) + 'deg)';
+	$("#hhand").css('transform', 'rotateZ(' + ((((hour % 12) * 30) % 360) + (minute / 2)) + 'deg)');
+	$("#mhand").css('transform', 'rotateZ(' + (minute * 6) + 'deg)');
 
-	var answers = [];
+	let answers = [];
 	answers.push(timeToText(hour, minute));
 	while(answers.length < 4) {
-		var newAnswer = fakeText(hour, minute);
-		var used = false;
-		for(var i = 0; i < answers.length; i++) {
+		let newAnswer = fakeText(hour, minute);
+		let used = false;
+		for(let i = 0; i < answers.length; i++) {
 			if(newAnswer == answers[i]) {
 				used = true;
 			}
@@ -22,11 +23,9 @@ function showTime(hour, minute) {
 
 	correct = answers[0];
 	shuffle(answers);
-	var answerFields = document.getElementsByClassName("answerText");
-	var radioButtons = document.getElementsByClassName("answer");
-	for(var i = 0; i < answers.length && i < answerFields.length; i++) {
-		radioButtons[i].value = answers[i];
-		answerFields[i].innerHTML = answers[i];
+	let radioButtons = $('.answer');
+	for(let i = 0; i < answers.length; i++) {
+		radioButtons.eq(i).val(answers[i]).css('opacity', 1);
 	}
 }
 
@@ -38,7 +37,7 @@ function shuffle(a) {
 }
 
 function fakeText(hour, minute) {
-	if(Math.random() < 0.25) {
+	if(Math.random() < 0.5) {
 		hour = Math.round(Math.abs((hour + (Math.random() * 48 - 24)) % 24));
 		minute = Math.round(Math.abs((minute + (Math.random() * 120 - 60)) % 60));
 		return timeToText(hour, minute);
@@ -51,7 +50,7 @@ function fakeText(hour, minute) {
 
 function timeToText(hour, minute) {
 	console.log("Hour: " + hour + " Minute: " + minute);
-	var numbers = ["twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine"];
+	let numbers = ["twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine"];
 	if(minute == 0) {
 		return numbers[hour] + " o'clock";
 	}
@@ -75,21 +74,22 @@ function timeToText(hour, minute) {
 }
 
 function generateNewTime() {
-	var hour = Math.round(Math.random()*24);
-	var minute = Math.round(Math.random()*60);
+	let hour = Math.round(Math.random()*12);
+	let minute = Math.round(Math.random()*60);
 	showTime(hour, minute);
 }
 
-document.getElementById("submitForm").addEventListener("click", function(e) {
-	e.preventDefault();
-	var selected = document.querySelector('input[name = "answer"]:checked');
-	var answer = selected.value;
+$('.answer').click(function(e) {
+	let selected = e.currentTarget;
+	let answer = selected.value;
 	if(correct == answer) {
 		generateNewTime();
-		selected.checked = false;
-		result.innerHTML = "Correct";
+		resultPositive.animate({'opacity': 1}, 400);
+		resultNegative.animate({'opacity': 0}, 400);
 	} else {
-		result.innerHTML = "So close! Try it one more time.";
+		$(selected).animate({'opacity': .3}, 200);
+		resultNegative.animate({'opacity': 1}, 400);
+		resultPositive.animate({'opacity': 0}, 400);
 	}
 });
 
